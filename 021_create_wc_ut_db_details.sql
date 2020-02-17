2295ee9f073c1c3d5c813967bdfde38c5af4fa6e
Adrian Ward <adrian.ward@addidici.com>
2020-02-17 17:12:20 +0000

    More

---
+++ 021_create_wc_ut_db_details.sql
@@ -0,0 +1,21 @@
+-- Author: Adrian Ward
+-- Date: Feb 2020
+-- Desc: Used to store the DB detail rows
+CREATE TABLE WC_UT_DB_DETAILS 
+   (	
+   	ID                   NUMBER(10,0), 
+	LOGICAL_QUERY_ID     VARCHAR2(50 BYTE) , 
+	QUERY_TEXT           VARCHAR2(1024 BYTE) , 
+	QUERY_BLOB           CLOB , 
+	TIME_SEC             NUMBER(10,0), 
+	ROW_COUNT            NUMBER(20,0), 
+	START_TS             DATE, 
+	START_DT             DATE, 
+	START_HOUR_MIN       CHAR(5 BYTE) , 
+	END_TS               DATE, 
+	END_DT               DATE, 
+	END_HOUR_MIN         CHAR(5 BYTE) , 
+	HASH_ID              VARCHAR2(128 BYTE) , 
+	PHYSICAL_HASH_ID      VARCHAR2(128 BYTE) COLLATE USING_NLS_COMP
+   )
+   ;
