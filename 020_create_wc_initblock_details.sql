261eed39e916802988a45d75fc1143f9fac1a5a4
Adrian Ward <adrian.ward@addidici.com>
2020-02-18 12:47:36 +0000

    UT Warehouse tables

--- 020_create_wc_initblock_details.sql
+++ 020_create_wc_initblock_details.sql
@@ -1,8 +1,9 @@
 -- Author: Adrian Ward
 -- Date: Feb 2020
 -- Desc: Used to store the DB detail rows
+DROP TABLE WC_UT_INITBLOCK_DETAILS;
 
-  CREATE TABLE WC_UT_INITBLOCK_DETAILS 
+CREATE TABLE WC_UT_INITBLOCK_DETAILS 
    (	
     USER_NAME           VARCHAR2(128 BYTE) , 
 	REPOSITORY_NAME     VARCHAR2(128 BYTE) , 
@@ -14,6 +15,8 @@
 	START_TS            TIMESTAMP (6), 
 	END_TS              TIMESTAMP (6), 
 	DURATION            NUMBER(13,3), 
-	NOTES               VARCHAR2(1024 BYTE) 
+	NOTES               VARCHAR2(1024 BYTE),
+	START_DT_WID            NUMBER(10,0),
+	USER_WID                NUMBER(10,0) 
    )  
  NOCOMPRESS NOLOGGING ;
